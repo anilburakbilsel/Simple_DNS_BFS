c8b3f5058642f6a6141d7c193ce5fc9df1d89d3b
anilburakbilsel <abbilsel@gmail.com>
2021-03-28 23:31:41 -0700

    example?

--- tree.js
+++ tree.js
@@ -1,8 +1,3 @@
-import {
-    reallyReallyLongModuleExportName as shortName,
-    anotherLongModuleName as short
-  } from '/modules/my-module.js';
-  
 import Node from './node'; 
 
 class Tree {
@@ -15,3 +10,23 @@ class Tree {
 // Use the class
 var myNode = new Node(50);
 myNode.mytest();
+
+
+
+class User {
+    constructor(name, age, email) {
+      this.name = name;
+      this.age = age;
+      this.email = email;
+    }
+  
+    getUserStats() {
+      return `
+        Name: ${this.name}
+        Age: ${this.age}
+        Email: ${this.email}
+      `;
+    }
+  }
+  
+  module.exports = User;
