e16d897c04c3135d2d3b2b2e1f0fb4431e8b53a4
anilburakbilsel <abbilsel@gmail.com>
2021-03-30 19:31:23 -0700

    commit 2

--- tree.js
+++ tree.js
@@ -9,4 +9,24 @@ class Tree {
 
 // Use the class
 var myNode = new Node(50);
-myNode.mytest();
+myNode.mytest()
+
+
+
+class User {
+    constructor(name, age, email) {
+      this.name = name;
+      this.age = age;
+      this.email = email;
+    }
+  
+    getUserStats() {
+      return `
+        Name: ${this.name}
+        Age: ${this.age}
+        Email: ${this.email}
+      `;
+    }
+  }
+  
+  module.exports = User;
