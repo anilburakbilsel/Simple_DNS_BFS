a263c7cabe92b9d5ddfaf98b9dd8019b3ebce806
anilburakbilsel <abbilsel@gmail.com>
2021-04-04 00:10:28 -0700

    some modifications are done

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
-This is going to be a very simple sorted binary tree implementation which contains only numbers.
+This is going to be a very simple binary tree implementation which contains only numbers. In short; a tree is a recursive data structure.
+
+A binary search (sometimes called an ordered or sorted binary tree) stores items in memory. It allows fast lookup, addition, and removal of items. It can be  used to implement lookup tables or dynamic set of items (for finding specified values/items). 
+
+By definition, a binary search tree does not contain any duplicate elements since all the node values in the left subtree must be smaller than their parent node while all the node values in the right tree must be bigger than the parent node (this also means that  binary search tree keep the values - or we can say the key values stored in each node - in sorted order). This lets us to use the principle of binary search.
+
+If the tree is balanced, most of the operations take only O(log n) time - which is much faster then linear operations/lookup in an unsorted array.
+
+In tree structure, each node has exactly one predecessor except the root, which has none. And keep in mind that all nodes are reachable from the root. In binary trees, each node can have at most two children. In this case, each node has exactly two pointers: one to the left subtree and one to the right subtree (one of them or both can be null).
+
+Keep in mind that, in a general tree, a node can have any number of child nodes.
 
 This code will also contain a simple Depth-First Search and Breadth-First Search.
+
+The space complexity of a binary search tree is O(n).
+
+Access, deletion, search and insertion are O(log n) as long as the tree is balanced.
+
+Trees are very useful for exposing the recursive structure of natural language and computer programs.
+
+(in some cases, it is useful to have trees in which nodes can refer their parents - maybe you can think of it as a doubly-linked list.)
--- node.js
+++ node.js
@@ -8,6 +8,9 @@ export default class Node {
 
     // in this simple case, lets limit the number of occurances
     add(value) {
+        if (typeof(value) != Number) {
+            throw new Error("only number values are allowed, please provide a number value, exiting...");
+        }
         if (!this.children.includes(value)) {
             if (value < this.data) {
                 if (this.left == null) {
@@ -32,8 +35,80 @@ export default class Node {
         }
     }
 
+    contains(value) {
+        if (value == this.data) {
+            return true;
+        }
+        else if (value < this.data) {
+            if (this.left == null) {
+                return false;
+            }
+            else {
+                return this.left.contains(value);
+            }
+        }
+        else {
+            if (this.right == null) {
+                return false;
+            }
+            else {
+                return this.right.contains(value);
+            }
+        }
+    }
+
+    inorderTraversal() {
+        if (this.left != null) {
+            this.inorderTraversal(this.left);
+        }
+        console.log(this.data);
+        if (this.right != null) {
+            this.inorderTraversal(this.right);
+        }
+    }
+
     remove(data) {
         this.children = this.children.filter(child => child.data !== data);
+    }
+
+
+    leftHeight() {
+        if (!this.left) {
+            return 0;
+        }
+        return this.left.height() + 1;
+    }
+
+    rightHeight() {
+        if (!this.right) {
+            return 0;
+        }
+        return this.right.height() + 1;
+    }
+
+    height() {
+        return Math.max(this.leftHeight, this.rightHeight);
+    }
+
+    balanceNumber() {
+        return this.leftHeight() - this.rightHeight();
+    }
+
+    balancedTree() {
+        if (Math.abs(this.balanceNumber()) > 1) {
+            console.log("the tree is unbalanced");
+            return false;
+        }
+        console.log("the tree is balanced");
+        return true;
+    }
+
+    // returns the total number of nodes
+    totalNumberOfNodes() {
+        if (this.data == null) {
+            return 0;
+        }
+        return this.left.totalNumberOfNodes() + this.right.totalNumberOfNodes() + 1;
     }
 
     mytest() {
